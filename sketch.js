3de94b5748d615ad300164f36767d47e6f328f2d
simranpradhan <92982366+simranpradhan@users.noreply.github.com>
2022-01-13 15:14:21 +0530

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -77,7 +77,7 @@ function setup() {
   button = createImg('cut_btn.png');
   button.position(200,320);
   button.size(50,50);
-
+  button.mouseClicked(remove_rope);
   button2 = createImg('cut_btn.png');
   button2.position(30,420);
   button2.size(50,50);
@@ -113,7 +113,7 @@ function draw()
 
   if(collide(fruit,bunny,80)==true)
   {
-   remove_rope();
+   //remove_rope();
    bubble.visible = false;
     World.remove(engine.world,fruit);
     fruit = null;
@@ -143,7 +143,11 @@ function drop()
   con2.dettach();
   con2 = null; 
 }
-
+//function drop2() {
+ // rope.break();
+  //con.dettach();
+  //con = null;
+//}
 function remove_rope()
 {
   rope.break();
